62d8cd939e11d4cd84826c916e27ddaac8b46d2f
Colin <colinbul@googlemail.com>
2016-12-09 09:50:48 +0000

    Tidying up comment removal ILxGen.fs

--- src/fsharp/IlxGen.fs
+++ src/fsharp/IlxGen.fs
@@ -6468,12 +6468,9 @@ and GenTypeDef cenv mgbuf lazyInitInfo eenv m (tycon:Tycon) =
                                                              callInstrs),
                                                           None))
                       yield ilMethodDef
-                  | None,_ ->
-                      ()
-                  | _,None ->
-                      ()
-                  | _ ->
-                      ()
+                  | None,_ -> ()
+                  | _,None -> ()
+                  | _ -> ()
               | _ -> () ]
               
         let ilMethods = methodDefs @ augmentOverrideMethodDefs @ abstractMethodDefs
