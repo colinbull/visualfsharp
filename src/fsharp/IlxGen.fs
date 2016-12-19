d50e0a6c084443721749d81a160d1330b12b249f
liboz <ringil.sword@gmail.com>
2016-12-19 13:51:49 -0500

    fix tests and an accessibility bug by using "%+A" instead of "%A"

--- src/fsharp/IlxGen.fs
+++ src/fsharp/IlxGen.fs
@@ -6456,7 +6456,7 @@ and GenTypeDef cenv mgbuf lazyInitInfo eenv m (tycon:Tycon) =
                                                          (true,[],2,
                                                           nonBranchingInstrsToCode 
                                                             ([ // load the hardwired format string
-                                                               yield I_ldstr "%A"  
+                                                               yield I_ldstr "%+A"  
                                                                // make the printf format object
                                                                yield mkNormalNewobj newFormatMethSpec
                                                                // call sprintf
@@ -6467,7 +6467,8 @@ and GenTypeDef cenv mgbuf lazyInitInfo eenv m (tycon:Tycon) =
                                                                   yield mkNormalLdobj ilThisTy  ] @
                                                              callInstrs),
                                                           None))
-                      yield ilMethodDef
+                      let mdef = { ilMethodDef with CustomAttrs = mkILCustomAttrs [ cenv.g.CompilerGeneratedAttribute ] }
+                      yield mdef
                   | None,_ -> ()
                   | _,None -> ()
                   | _ -> ()
